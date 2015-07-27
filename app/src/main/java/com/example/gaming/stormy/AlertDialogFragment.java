74fc4d37a9d701b19fb4ff5ba8e9849f6e8e3f3f
Jacob <jacob.phong.le@gmail.com>
2015-07-27 12:22:33 -1000

    WIth data retrieve from forecast.io implemented new images

---
+++ app/src/main/java/com/example/gaming/stormy/AlertDialogFragment.java
@@ -0,0 +1,23 @@
+package com.example.gaming.stormy;
+
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Gaming on 7/26/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
